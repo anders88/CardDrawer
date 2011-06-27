c164e0592a7b018ab58a64c81c093a6eb9f5c60f
Anders Karlsen <karlsenanders@hotmail.com>
2011-06-27 12:55:04 +0200

    First version of drawing cards

--- src/main/java/no/anksoft/CardDrawer/CardDealer.java
+++ src/main/java/no/anksoft/CardDrawer/CardDealer.java
@@ -1,16 +1,31 @@
 package no.anksoft.CardDrawer;
 
+import java.util.Random;
+
 
 public class CardDealer {
 
 	private final int highest;
+	private Random random;
+	private int cardsLeft;
 
 	public CardDealer(int highest) {
 		this.highest = highest;
+		this.cardsLeft = highest;
 	}
 
 	public int numberOfCardsInDrawpile() {
-		return highest;
+		return cardsLeft;
+	}
+
+	public void setRandom(Random random) {
+		this.random = random;
+		
+	}
+
+	public int drawCard() {
+		cardsLeft--;
+		return random.nextInt(highest)+1;
 	}
 
 }
--- src/test/java/no/anksoft/CardDrawer/CardDrawerTest.java
+++ src/test/java/no/anksoft/CardDrawer/CardDrawerTest.java
@@ -1,13 +1,27 @@
 package no.anksoft.CardDrawer;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Random;
 
 import org.junit.Test;
 
 public class CardDrawerTest {
+	private CardDealer cardDealer = new CardDealer(10);
+
 	@Test
 	public void shouldHaveAllCardsInDrawPileFromTheStart() throws Exception {
-		CardDealer cardDealer = new CardDealer(10);
 		assertThat(cardDealer.numberOfCardsInDrawpile()).isEqualTo(10);
+	}
+	
+	@Test
+	public void shouldDrawACard() throws Exception {
+		Random random = mock(Random.class);
+		cardDealer.setRandom(random);
+		when(random.nextInt(10)).thenReturn(3);
+		assertThat(cardDealer.drawCard()).isEqualTo(4);
+		assertThat(cardDealer.numberOfCardsInDrawpile()).isEqualTo(9);
 	}
 }
