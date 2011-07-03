98ae2bc8de7967b5cedf775c069bf9b62e9f0bf7
Anders Karlsen <karlsenanders@hotmail.com>
2011-07-03 16:25:27 +0200

    Stuff setting up web server

---
+++ src/main/java/no/anksoft/carddrawer/CardDrawerServlet.java
@@ -0,0 +1,9 @@
+package no.anksoft.carddrawer;
+
+import javax.servlet.http.HttpServlet;
+
+public class CardDrawerServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -100236375084824924L;
+
+}
---
+++ src/test/java/no/anksoft/carddrawer/CardDrawerWebTest.java
@@ -0,0 +1,46 @@
+package no.anksoft.carddrawer;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class CardDrawerWebTest {
+	@Test
+	@Ignore
+	public void shouldBeAbleToLogInAndSeeStatusScreeen() throws Exception {
+		Server server = new Server(0);
+		server.addHandler(new WebAppContext("src/main/webapp", "/"));
+		server.start();
+		int localPort = server.getConnectors()[0].getLocalPort();
+		String baseUrl = "http://localhost:" + localPort + "/";
+		WebDriver browser = createBrowser();
+		browser.get(baseUrl);
+		browser.findElement(By.linkText("Login")).click();
+		browser.findElement(By.name("player_name")).sendKeys("Darth");
+		browser.findElement(By.name("loginPlayer")).click();
+		
+		assertThat(browser.getPageSource()).contains("Player Darth");
+		assertThat(browser.getCurrentUrl()).contains("cardDrawer/status");
+	}
+
+	private HtmlUnitDriver createBrowser() {
+		return new HtmlUnitDriver() {
+			@Override
+			public WebElement findElement(By by) {
+				try {
+					return super.findElement(by);
+				} catch (NoSuchElementException e) {
+					throw new NoSuchElementException("Did not find " + by + " in " + getPageSource());
+				}
+			}
+		};
+	}
+}
